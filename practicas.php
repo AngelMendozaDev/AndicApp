5b1b3d644a672dd4f981e3098ccbfb4a12c5a50a
AngelDevPC <ing.Angel_mendoza@outlook.com>
2022-07-27 07:55:00 -0500

    Practicas (10%)

--- practicas.php
+++ practicas.php
@@ -4,7 +4,7 @@ require_once "classes/funciones.php";
 $model = new Procedures();
 $result = $model->getPersons();
 ?>
-
+<link rel="stylesheet" href="static/css/comunity.css">
 <link rel="stylesheet" href="static/css/practicas.css">
 
 <div class="cont-general">
@@ -87,4 +87,200 @@ $result = $model->getPersons();
     </div>
 </div>
 
-<?php require_once "footer.php" ?>
+<!-- Modal New/ Update -->
+<div class="modal fade" id="ModalControl" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="ModalControlLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="ModalControlLabel">Control de registro</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form method="POST" onsubmit="return setPerson()" class="needs-validation" id="form-person">
+                    <h5 id="title-form">some</h5>
+                    <input type="text" id="person" name="person" hidden>
+                    <div class="cont-form">
+                        <!-- Parte Info Personal -->
+                        <div class="part" id="part-0">
+                            <div class="input-group mb-3">
+                                <span class="input-group-text">Nombre:</span>
+                                <input type="text" class="form-control c-mayusc" name="nombre" id="nombre" placeholder="Nombre(s)" maxlength="30" required>
+                            </div>
+                            <div class="form-flex">
+                                <div class="input-group mb-3">
+                                    <span class="input-group-text">Ap. Paterno:</span>
+                                    <input type="text" class="form-control c-mayusc" maxlength="25" placeholder="Apellido Paterno" name="app" id="app" required>
+                                </div>
+                                <div class="input-group mb-3">
+                                    <span class="input-group-text">Ap. Materno:</span>
+                                    <input type="text" class="form-control c-mayusc" maxlength="25" placeholder="Apellido Materno" name="apm" id="apm" required>
+                                </div>
+                            </div>
+                            <div class="input-group mb-3">
+                                <span class="input-group-text">
+                                    <i class="fa fa-envelope" aria-hidden="true"></i>
+                                    &nbsp;
+                                    Correo:
+                                </span>
+                                <input type="email" class="form-control" name="mail" id="mail" placeholder="someMail@andic.org.mx" maxlength="60" required>
+                            </div>
+                            <div class="form-flex">
+                                <div class="input-group h-100 mb-3 cont-sex">
+                                    <span class="input-group-text">Sexo:</span>
+                                    <div class="sex-group mx-auto">
+                                        <input type="radio" name="sex" value="F" class="myCampo" id="sex-f" required>
+                                        <label for="sex-f">
+                                            <i class="fa fa-female mr-5" aria-hidden="true"></i>
+                                        </label>
+                                    </div>
+                                    <div class="sex-group mx-auto">
+                                        <input type="radio" name="sex" value="M" class="myCampo" id="sex-m" required>
+                                        <label for="sex-m">
+                                            <i class="fa fa-male" aria-hidden="true"></i>
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="input-group mb-3">
+                                    <span class="input-group-text">
+                                        <i class="fa fa-calendar-check" aria-hidden="true"></i>
+                                    </span>
+                                    <input type="date" class="form-control" name="nacimiento" id="nacimiento" required>
+                                </div>
+                                <div class="input-group mb-3">
+                                    <span class="input-group-text">
+                                        <i class="fab fa-whatsapp" aria-hidden="true"></i>
+                                    </span>
+                                    <input type="text" class="form-control" name="phone" id="phone" placeholder="Número a 10 digitos" required>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Parte de Domicilio -->
+                        <div class="part" id="part-1">
+                            <div class="input-group mb-3">
+                                <span class="input-group-text">Calle:</span>
+                                <input type="text" class="form-control" name="calle" id="calle" style="text-transform: uppercase;" maxlength="60" required>
+                            </div>
+                            <div class="form-flex">
+                                <div class="input-group mb-3">
+                                    <span class="input-group-text">Código Postal:</span>
+                                    <input type="text" class="form-control" name="cp" id="cp" required>
+                                </div>
+                                <div class="input-group mb-3">
+                                    <span class="input-group-text">Colonia:</span>
+                                    <select name="colonia" id="colonia" class="form-select" required>
+                                        <option value="" selected="true" disabled> Selecciona una opcion...</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-flex">
+                                <div class="input-group mb-3">
+                                    <span class="input-group-text">Alcaldía:</span>
+                                    <input type="text" class="form-control" name="alc" id="alc" readonly required>
+                                </div>
+                                <div class="input-group mb-3">
+                                    <span class="input-group-text">Estado:</span>
+                                    <input type="text" class="form-control" name="edo" id="edo" readonly required>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="school-info">
+                        <div class="input-groups mb-3">
+                            <label for="school-text" class="form-label">Institución educativa:</label>
+                            <input class="form-control" list="datalistOptions" id="school-text" placeholder="Type to search...">
+                            <datalist id="datalistOptions">
+                                <option value="San Francisco">
+                                <option value="New York">
+                                <option value="Seattle">
+                                <option value="Los Angeles">
+                                <option value="Chicago">
+                            </datalist>
+                        </div>
+
+                        <div class="input-groups mb-3">
+                            <label for="carrera-text" class="form-label">Carrera:</label>
+                            <input class="form-control" list="carrera-options" id="carrera-text" placeholder="Type to search...">
+                            <datalist id="carrera-options">
+                                <option value="San Francisco">
+                                <option value="New York">
+                                <option value="Seattle">
+                                <option value="Los Angeles">
+                                <option value="Chicago">
+                            </datalist>
+                        </div>
+
+                        <div class="form-floating">
+                            <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
+                                <option selected>Selecciona tu grado</option>
+                                <option value="1">1°</option>
+                                <option value="2">2°</option>
+                                <option value="3">3°</option>
+                                <option value="4">4°</option>
+                                <option value="5">5°</option>
+                                <option value="6">6°</option>
+                                <option value="7">7°</option>
+                                <option value="8">8°</option>
+                                <option value="9">9°</option>
+                                <option value="10">10°</option>
+                                <option value="11">11°</option>
+                                <option value="12">12°</option>
+                                <option value="13">13°</option>
+                                <option value="14">14°</option>
+                                <option value="15">15°</option>
+                                <option value="16">16°</option>
+                            </select>
+                            <label for="floatingSelect">Semestre / Cuatrimestre Cursado</label>
+                        </div>
+                    </div>
+                    <div class="control-form">
+                        <button type="submit" class="btn btn-success" id="btn-send">
+                            <i class="fa fa-paper-plane" aria-hidden="true"></i>
+                            Enviar
+                        </button>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-bs-dismiss="modal" id="closeModal">Cerrar</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!-- Modal Image -->
+<div class="modal fade" id="pictureModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="pictureModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="pictureModalLabel">Gestor de Imagen</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="image-box">
+                    <img src="" alt="" id="imagePerson">
+                </div>
+                <hr>
+                <form method="POST" id="changeImageForm" onsubmit="return setImage()" enctype="multipart/form-data">
+                    <input type="text" name="persona" id="folP" hidden>
+                    <div class="input-group">
+                        <input type="file" name="picture" id="foto" class="form-control" required>
+                    </div>
+                    <br>
+                    <center>
+                        <button type="submit" id="btn-foto" class="btn btn-success">
+                            <i class="fas fa-sync"></i>
+                            &nbsp;
+                            Actualizar imagen
+                        </button>
+                    </center>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cerrar</button>
+            </div>
+        </div>
+    </div>
+
+    <?php require_once "footer.php" ?>
